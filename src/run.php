3db8bf10f453a7b505feb8abb9502c3482113c13
serhii-kanin <40164553+serhii-kanin@users.noreply.github.com>
2019-05-22 12:05:01 +0300

    Update run.php

--- src/run.php
+++ src/run.php
@@ -3,7 +3,7 @@
 include "../vendor/autoload.php";
 
 /**
- * function testFunc(): void { int sukaint = !1 + 1; int suka2 = 1;return suka2 + sukaint;}
+ * function testFunc(): void { int varint = !1 + 1; int var1 = 1;return var1 + varint;}
 string stringVar = "12332\"31";
 bool boolVar = false;
 int false1 = 2;
@@ -11,10 +11,10 @@ float float1 = 2.2;
 if (1 > baz && 3 < 2 && 1 || true) {
 while(4 > 1 && 2 > 1 || 3 < 2) {
 if(1 * 2 + 3 != 10 + 3 + 5 * 2) {
-bool suka = 1+1;
+bool var22 = 1+1;
 }
 }
-} else { bool suka = 1 + 2;}');
+} else { bool var22 = 1 + 2;}');
  */
 
 $lexer = new \SK\Compiler\Lexer('1 + 1 - 1 - 2 * 3 * 4 * 5;');
